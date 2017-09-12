2317ff22f5c8c9ac9a9f18594a9b65b3fe693a5c
Alexey Zimarev <alex@zimarev.com>
2017-09-12 12:24:10 +0200

    Initial commit

---
+++ Demo.MassTransit.Service1/Program.cs
@@ -0,0 +1,25 @@
+﻿using Serilog;
+using Topshelf;
+
+namespace Demo.MassTransit
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            ConfigureLogging();
+            
+            HostFactory.Run(x =>
+            {
+                x.UseSerilog();
+                x.Service<Service>();
+            });
+        }
+        
+        private static void ConfigureLogging() =>
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.LiterateConsole()
+                .CreateLogger();
+    }
+}
